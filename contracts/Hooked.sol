b5211bcf8ef108900bc08c7eae1070057041921f
Spencer Miller <spence.miller@gmail.com>
2021-09-07 10:08:11 -0700

    add primary contract

--- contracts/Hooked.sol
+++ contracts/Hooked.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Hooked {
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Hooked is ERC1155 {
   constructor() public {
+  }
+
+  function fish(
+    address account,
+    bytes memory data
+  ) public {
+    
+    _mint(account, catchType, 1, data);
   }
 }
